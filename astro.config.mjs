acd4cb97558c3481deab064e7192c39d22899fd9
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2024-10-09 21:32:25 +0200

    add deploy script

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-
-// Import the Vercel adapter
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel(),
+  site: "https://autospeise.methebe.com",
 });
